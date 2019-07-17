5e16272a0cac40d7bf520df424c6efbfb28361ff
StephaneSmail <52791966+StephaneSmail@users.noreply.github.com>
2019-07-17 16:51:01 +0200

    4

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+{
+/**
+ * @Route("/session")
+ */
+class SessionController extends AbstractController
+{
+    /**
+     * @Route("/", name="session_index", methods={"GET"})
+     */
+    public function index(SessionRepository $sessionRepository): Response
+    {
+        return $this->render('session/index.html.twig', [
+            'sessions' => $sessionRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="session_new", methods={"GET","POST"})
+     */
+    public function new(Request $request,ObjectManager $manager): Response
+    {
+        $formation = new Formation();
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($formation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('formation_index');
+        }
+
+        return $this->render('formation/new.html.twig', [
+            'formation' => $formation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="formation_show", methods={"GET"})
+     */
+    public function show(Formation $formation): Response
+
+    {
+
+
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire,
+             
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="formation_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Formation $formation, ObjectManager $manager): Response
+    {
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            return $this->redirectToRoute('formation_index', [
+                'id' => $formation->getId(),
+            ]);
+        }
+
+        return $this->render('formation/edit.html.twig', [
+            'formation' => $formation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="formation_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Formation $formation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$formation->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($formation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('formation_index');
+        
+    }
