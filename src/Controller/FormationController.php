54e4a3bbc8c200b3c1cb118a64bfcd4cfd64b85c
StephaneSmail <52791966+StephaneSmail@users.noreply.github.com>
2019-07-17 15:27:28 +0200

    Formation CRUD

---
+++ src/Controller/FormationController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Controller;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use App\Entity\Formation;
+use App\Form\FormationType;
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+
+
+// Include Dompdf required namespaces
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/formation")
+ */
+class FormationController extends AbstractController
+{
+    /**
+     * @Route("/", name="formation_index", methods={"GET"})
+     */
+    public function index(FormationRepository $formationRepository): Response
+    {
+        return $this->render('formation/index.html.twig', [
+            'formations' => $formationRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="formation_new", methods={"GET","POST"})
+     */
+    public function new(Request $request,ObjectManager $manager): Response
+    {
+        $formation = new Formation();
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($formation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('formation_index');
+        }
+
+        return $this->render('formation/new.html.twig', [
+            'formation' => $formation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="formation_show", methods={"GET"})
+     */
+    public function show(Formation $formation): Response
+
+    {
+
+
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire,
+             
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="formation_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Formation $formation, ObjectManager $manager): Response
+    {
+        $form = $this->createForm(FormationType::class, $formation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            return $this->redirectToRoute('formation_index', [
+                'id' => $formation->getId(),
+            ]);
+        }
+
+        return $this->render('formation/edit.html.twig', [
+            'formation' => $formation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="formation_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Formation $formation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$formation->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($formation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('formation_index');
+        
+    }
+
+    /**
+     * @Route("/pdf/{id}", name="formation_pdf", methods={"GET"})
+     */
+    public function formationPdf(formation $formation)
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('formation/mypdf.html.twig', [
+            'formation' => $formation
+        ]);
+        
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+}
+
