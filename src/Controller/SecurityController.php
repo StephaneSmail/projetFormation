6ccafbbbc25342c1827ae65e04cbef1b052fa24c
Flo <florentob@gmail.com>
2019-07-25 13:52:29 +0200

    amménagement

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,7 +17,7 @@ use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
